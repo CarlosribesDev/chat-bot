76db440bc1d5b4148f0e6d05c2a10006d6edbdc5
carlosribesDev <carlosribes.dev@gmail.com>
2025-02-22 21:31:44 +0100

    twilio test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, Body, Req, Res, UnauthorizedException } from '@nestjs/common';
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { validateRequest } from 'twilio/lib/webhooks/webhooks';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+
+  @Post('webhook')
+  handleWhatsAppMessage(
+    @Body() body: any,
+    @Req() req: FastifyRequest,
+    @Res() res: FastifyReply,
+  ) {
+
+    const twilioSignature = req.headers['x-twilio-signature'];
+
+    // Verificar que la firma sea una cadena
+    if (typeof twilioSignature !== 'string') {
+      throw new UnauthorizedException('Firma no válida');
+    }
+
+
+    const authToken = process.env.TWILIO_AUTH_TOKEN || "";
+    const url = 'https://your-ngrok-url/webhook';
+    const params = body;
+
+    console.log(authToken)
+
+    const isValid = validateRequest(
+      authToken,
+      twilioSignature,
+      url,
+      params,
+    );
+
+    if (!isValid) {
+      throw new UnauthorizedException('Firma no válida');
+    }
+
+    console.log('Mensaje recibido de WhatsApp:', body);
+
+
+    const messageBody = body.Body;
+    const fromNumber = body.From;
+
+    console.log(`Mensaje: ${messageBody} | De: ${fromNumber}`);
+
+    res.header('Content-Type', 'text/xml');
+    res.send(`
+      <Response>
+        <Message>¡Hola! Recibimos tu mensaje: "${messageBody}".</Message>
+      </Response>
+    `);
   }
 }
